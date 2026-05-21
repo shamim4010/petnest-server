91b87af7ca82fefd15c76e0fde0b12f4a5a23ca2
shamim4010 <shamimislam401017@gmail.com>
2026-05-21 12:25:30 +0600

    add order management endpoints and restructure code

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const { MongoClient, ServerApiVersion} = require('mongodb');
 dotenv.config();
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 const uri  = process.env.MONGODB_URI;
 const PORT = process.env.PORT
 
@@ -23,17 +26,31 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
     const db = client.db('PetNest');
     const petss = db.collection('pets');
+    const petsOrder = db.collection('orders');
+
     app.get('/pets', async(req, res) => {
       const cursor = petss.find();
       const results = await cursor.toArray();
       res.send(results);
     })
 
-    app.get(('/pets/:id'), async(req, res) =>{
+    app.get('/pets/:id', async(req, res) =>{
         const id = parseInt(req.params.id);
         const query = {id : id};
         const results = await petss.findOne(query);
         res.send(results);
+    })
+
+    app.get('/orders/:userId', async(req, res) => {
+      const {userId} = req.params
+      const results = await petsOrder.find({ userId: userId }).toArray();
+      res.json(results)
+    })
+
+    app.post('/orders', async(req, res) => {
+      const orderPets = req.body
+      const result = await petsOrder.insertOne(orderPets).toArray()
+      res.json(result);
     })
 
   } finally {
